a8dd441a058b5f597e7f4d8020f8b027451b646a
flashback2k14 <yeahdev@gmail.com>
2021-07-31 22:01:50 +0200

    initial commit

---
+++ api/training/data.js
@@ -0,0 +1,34 @@
+const { notion, checkAuth } = require('../../utils');
+
+module.exports = async (req, res) => {
+  if (!checkAuth(req.headers)) {
+    res.status(401).send('NO AUTH');
+  }
+
+  const { results: pages } = await notion.databases.query({
+    database_id: process.env.NOTION_DB_ID,
+    sorts: [
+      {
+        property: 'ID',
+        direction: 'ascending',
+      },
+    ],
+  });
+
+  const results = pages.map((page) => {
+    const result = {
+      pid: page.id,
+      rid: page.properties['ID']?.title[0]?.plain_text ?? -1,
+      date: page.properties['Date']?.date?.start ?? -1,
+      time: page.properties['Time']?.rich_text[0]?.plain_text ?? -1,
+      spm: page.properties['SPM']?.number ?? -1,
+      distance: page.properties['Distance']?.number ?? -1,
+      totalStrokes: page.properties['Total Strokes']?.number ?? -1,
+      calories: page.properties['Calories']?.number ?? -1,
+      recovery: page.properties['Recovery']?.rich_text[0]?.plain_text ?? -1,
+    };
+    return result;
+  });
+
+  res.json(results);
+};
---
+++ utils/index.js
@@ -0,0 +1,21 @@
+const dotEnv = require('dotenv');
+const { Client } = require('@notionhq/client');
+
+dotEnv.config();
+const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
+
+const checkAuth = (headers) => {
+  if ('authorization' in headers) {
+    const authHeader = headers['authorization'];
+    const base64UserCred = authHeader.split(' ')[1];
+    const userCred = Buffer.from(base64UserCred, 'base64').toString();
+    const username = userCred.split(':')[0];
+    const pw = userCred.split(':')[1];
+    return username === process.env.CRED_USERNAME && pw === process.env.CRED_PW;
+  }
+
+  return false;
+};
+
+module.exports.notion = notion;
+module.exports.checkAuth = checkAuth;
